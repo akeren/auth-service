e798d85f60ef0dd6684ecb7072b5acd5cfa951fb
Kater Akeren <akerenkater@gmail.com>
2022-02-16 06:33:13 +0100

    Logout

--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -3,10 +3,12 @@ import { Router, Request, Response } from 'express';
 const router: Router = Router();
 
 router.post('/api/users/logout', (req: Request, res: Response) => {
+  req.session = null;
+
   res.status(200).json({
     status: true,
-    code: 200,
-    message: 'Welcome to the API',
+    code: res.statusCode,
+    message: 'Logged out successfully.',
     data: null,
   });
 });
