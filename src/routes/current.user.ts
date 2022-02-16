ce8b305d672aedae8afa2444161a99e0a1d4a737
Kater Akeren <akerenkater@gmail.com>
2022-02-16 06:27:21 +0100

    Retrieved me

--- src/routes/current.user.ts
+++ src/routes/current.user.ts
@@ -1,14 +1,36 @@
 import { Request, Response, Router } from 'express';
+import jwt from 'jsonwebtoken';
+import { UnauthorizedError } from '@src/errors';
+import { jwtConfig } from '@src/config';
+import { User } from '@src/models/user.model';
 
 const router: Router = Router();
 
-router.get('/api/users/me', (req: Request, res: Response) => {
-  res.status(200).json({
-    status: true,
-    code: 200,
-    message: 'Welcome to the API',
-    data: null,
-  });
+router.get('/api/users/me', async (req: Request, res: Response) => {
+  if (!req.session?.jwt) {
+    throw new UnauthorizedError('You are not logged in! Please login to get access.');
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, jwtConfig.secret);
+
+    const { id } = payload as { id: string };
+
+    const user = await User.findById(id);
+
+    if (!user) {
+      throw new UnauthorizedError('The user belonging to this token does no longer exist.');
+    }
+
+    return res.status(200).json({
+      success: true,
+      code: res.statusCode,
+      message: 'User profile successfully retrieved.',
+      data: user,
+    });
+  } catch (error) {
+    throw new UnauthorizedError('You are not logged in! Please login to get access.');
+  }
 });
 
 export { router as currentUserRouter };
