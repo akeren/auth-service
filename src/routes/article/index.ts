0c074f7c17d1a10439413b9980e1a0f8e536b070
Kater Akeren <akerenkater@gmail.com>
2023-06-29 22:28:42 +0100

    Get article

--- src/routes/article/index.ts
+++ src/routes/article/index.ts
@@ -1,4 +1,5 @@
 import { createArticleRouter } from './create-article.route';
 import { getAllArticlesRouter } from './get-all-articles.route';
+import { getArticleRouter } from './get-article.route';
 
-export { createArticleRouter, getAllArticlesRouter };
+export { createArticleRouter, getAllArticlesRouter, getArticleRouter };
